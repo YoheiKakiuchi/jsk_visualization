9f1453c8476e6fc352e5dc4e203b794456a17dd1
Yohei Kakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2020-04-18 06:56:40 +0900

    [jsk_rqt_plugins] fix buttons, read params collectly

--- jsk_rqt_plugins/src/jsk_rqt_plugins/button_general.py
+++ jsk_rqt_plugins/src/jsk_rqt_plugins/button_general.py
@@ -100,17 +100,31 @@ class ServiceButtonGeneralWidget(QWidget):
         self.button_type = button_type
         self._layout_param = None
         self._dialog = LineEditDialog()
+        self._no_restore = rospy.get_param('~no_restore', False)
+
+        if rospy.has_param("~layout_param"):
+            layout_yaml_string = rospy.get_param("~layout_param")
+            yaml_data = yaml.load(layout_yaml_string)
+            self.setupButtonsFromData(yaml_data)
+            rospy.loginfo("setup Buttons from ~layout_param")
+        elif rospy.has_param("~layout_yaml_file"):
+            layout_yaml_file = rospy.get_param("~layout_yaml_file")
+            self.loadLayoutYamlFile(layout_yaml_file)
+            rospy.loginfo("setup Buttons from ~layout_yaml_file")
+
         self.show()
 
     def showError(self, message):
         QMessageBox.about(self, "ERROR", message)
 
-    def loadLayoutYaml(self, layout_param):
+    def loadLayoutYamlFile(self, layout_yaml_file):
         # Initialize layout of the buttons from yaml file
         # The yaml file can be specified by rosparam
-        layout_yaml_file = rospy.get_param("~layout_yaml_file", layout_param)
-        resolved_layout_yaml_file = get_filename(
-            layout_yaml_file)[len("file://"):]
+        if layout_yaml_file in 'package://':
+            resolved_layout_yaml_file = get_filename(
+                layout_yaml_file)[len("file://"):]
+        else:
+            resolved_layout_yaml_file = layout_yaml_file
         # check file exists
         if not os.path.exists(resolved_layout_yaml_file):
             self.showError("Cannot find %s (%s)" % (
@@ -118,6 +132,85 @@ class ServiceButtonGeneralWidget(QWidget):
             sys.exit(1)
         self.setupButtons(resolved_layout_yaml_file)
         self.show()
+
+    def setupButtonsFromData(self, yaml_data):
+        """
+        Parse yaml file and setup Buttons. Format of the yaml file should be:
+        - name: 'button name' (required)
+          image: 'path to image for icon' (optional)
+          image_size: 'width and height of icon' (optional)
+          service: 'service' (required)
+          column: 'column index' (optional, defaults to 0)
+        """
+        self.buttons = []
+        # lookup colum direction
+        direction = 'vertical'
+        for d in yaml_data:
+            if d.has_key('direction'):
+                if d['direction'] == 'horizontal':
+                    direction = 'horizontal'
+                else: # d['direction'] == 'vertical':
+                    direction = 'vertical'
+                yaml_data.remove(d)
+                break
+        # lookup column num
+        column_indices = [d['column'] for d in yaml_data]
+        max_column_index = max(*column_indices)
+        if direction == 'vertical':
+            self.layout = QHBoxLayout()
+            self.layout_boxes = [QVBoxLayout()
+                                 for i in range(max_column_index + 1)]
+        else:  # direction == 'horizontal'
+            self.layout = QVBoxLayout()
+            self.layout_boxes = [QHBoxLayout()
+                                 for i in range(max_column_index + 1)]
+        self.button_groups = [QGroupBox()
+                              for i in range(max_column_index + 1)]
+        for button_data in yaml_data:
+            # check if all the field is available
+            if not button_data.has_key("name"):
+                self.showError("name field is missed in yaml")
+                raise Exception("name field is missed in yaml")
+            if not button_data.has_key("service"):
+                self.showError("service field is missed in yaml")
+                raise Exception("service field is missed in yaml")
+            if self.button_type == "push":
+                button = QToolButton()
+            else:  # self.button_type == "Radio":
+                button = QRadioButton()
+            button.setSizePolicy(
+                QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred))
+            if button_data.has_key("image"):
+                image_file = get_filename(
+                    button_data["image"])[len("file://"):]
+                if os.path.exists(image_file):
+                    icon = QtGui.QIcon(image_file)
+                    button.setIcon(icon)
+                    if button_data.has_key("image_size"):
+                        button.setIconSize(QSize(
+                            button_data["image_size"][0],
+                            button_data["image_size"][1]))
+                    else:
+                        button.setIconSize(QSize(100, 100))
+            if button_data.has_key("name"):
+                name = button_data['name']
+                button.setText(name)
+            button.clicked.connect(
+                self.buttonCallback(button_data['service']))
+            if self.button_type == "push":
+                button.setToolButtonStyle(
+                    QtCore.Qt.ToolButtonTextUnderIcon)
+            else:  # self.button_type == "Radio":
+                if button_data.has_key("default_value") and \
+                   button_data['default_value']:
+                    button.setChecked(True)
+            self.layout_boxes[button_data['column']].addWidget(button)
+            self.buttons.append(button)
+        for i in range(len(self.button_groups)):
+            self.button_groups[i].setLayout(self.layout_boxes[i])
+        for group in self.button_groups:
+            self.layout.addWidget(group)
+        self.setLayout(self.layout)
 
     def setupButtons(self, yaml_file):
         """
@@ -131,74 +224,7 @@ class ServiceButtonGeneralWidget(QWidget):
         self.buttons = []
         with open(yaml_file) as f:
             yaml_data = yaml.load(f)
-            # lookup colum direction
-            direction = 'vertical'
-            for d in yaml_data:
-                if d.has_key('direction'):
-                    if d['direction'] == 'horizontal':
-                        direction = 'horizontal'
-                    else: # d['direction'] == 'vertical':
-                        direction = 'vertical'
-                    yaml_data.remove(d)
-                    break
-            # lookup column num
-            column_indices = [d['column'] for d in yaml_data]
-            max_column_index = max(*column_indices)
-            if direction == 'vertical':
-                self.layout = QHBoxLayout()
-                self.layout_boxes = [QVBoxLayout()
-                                     for i in range(max_column_index + 1)]
-            else:  # direction == 'horizontal'
-                self.layout = QVBoxLayout()
-                self.layout_boxes = [QHBoxLayout()
-                                     for i in range(max_column_index + 1)]
-            self.button_groups = [QGroupBox()
-                                  for i in range(max_column_index + 1)]
-            for button_data in yaml_data:
-                # check if all the field is available
-                if not button_data.has_key("name"):
-                    self.showError("name field is missed in yaml")
-                    raise Exception("name field is missed in yaml")
-                if not button_data.has_key("service"):
-                    self.showError("service field is missed in yaml")
-                    raise Exception("service field is missed in yaml")
-                if self.button_type == "push":
-                    button = QToolButton()
-                else:  # self.button_type == "Radio":
-                    button = QRadioButton()
-                button.setSizePolicy(
-                    QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred))
-                if button_data.has_key("image"):
-                    image_file = get_filename(
-                        button_data["image"])[len("file://"):]
-                    if os.path.exists(image_file):
-                        icon = QtGui.QIcon(image_file)
-                        button.setIcon(icon)
-                        if button_data.has_key("image_size"):
-                            button.setIconSize(QSize(
-                                button_data["image_size"][0],
-                                button_data["image_size"][1]))
-                        else:
-                            button.setIconSize(QSize(100, 100))
-                if button_data.has_key("name"):
-                    name = button_data['name']
-                    button.setText(name)
-                button.clicked.connect(
-                    self.buttonCallback(button_data['service']))
-                if self.button_type == "push":
-                    button.setToolButtonStyle(
-                        QtCore.Qt.ToolButtonTextUnderIcon)
-                else:  # self.button_type == "Radio":
-                    if button_data.has_key("default_value") and \
-                       button_data['default_value']:
-                        button.setChecked(True)
-                self.layout_boxes[button_data['column']].addWidget(button)
-                self.buttons.append(button)
-            for i in range(len(self.button_groups)):
-                self.button_groups[i].setLayout(self.layout_boxes[i])
-            for group in self.button_groups:
-                self.layout.addWidget(group)
-            self.setLayout(self.layout)
+            self.setupButtonsFromData(yaml_data)
 
     def buttonCallback(self, service_name):
         """
@@ -219,14 +245,17 @@ class ServiceButtonGeneralWidget(QWidget):
             rospy.loginfo("save setting is called. %s" % self._layout_param)
 
     def restore_settings(self, plugin_settings, instance_settings):
-        if instance_settings.value("layout_param"):
+        if instance_settings.value("layout_param") and (not self._no_restore):
             self._layout_param = instance_settings.value("layout_param")
-            self.loadLayoutYaml(self._layout_param)
+            self.loadLayoutYamlFile(self._layout_param)
             rospy.loginfo("restore setting is called. %s" % self._layout_param)
 
     def trigger_configuration(self):
         self._dialog.exec_()
-        self._layout_param = self._dialog.value
-        self.loadLayoutYaml(self._layout_param)
-        rospy.loginfo(
-            "trigger configuration is called. %s" % self._dialog.value)
+        l_param = self._dialog.value
+        if rospy.has_param:
+            l_param = rospy.get_param(self._dialog.value)
+
+        self._layout_param = l_param
+        self.loadLayoutYamlFile(self._layout_param)
+        rospy.loginfo("trigger configuration is called. %s" % self._layout_param)
